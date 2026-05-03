686c3c391f43f066e7e3e317ad96ff296edaa11a
Filipe <piracicabafilipe@gmail.comdir>
2026-05-02 21:05:56 -0300

    7th commit

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import streamlit as st
 
 st.header('Flipping a coin')
 
+# adding the slider and button to the program:
+
+number_of_trials = st.slider('Number of try?', 1, 1000, 10)
+
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Executing the experiment with {number_of_trials} tryes.')
+
+
+
 st.write('still not as app. We are working on it.')
 
 
